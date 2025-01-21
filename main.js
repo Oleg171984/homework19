158ebe2bb1491f8f140be01264fd99cbd5aec9c3
Oleg171984 <kucugurnyj171984@gmail.com>
2025-01-21 22:49:44 +0200

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const apiKey = '8743029326bb6fb3381f059a850e0ae7'; 
+const apiKey = '8743029326bb6fb3381f059a850e0ae7';
 const weatherInfoDiv = document.getElementById('weather-info');
 const citySelect = document.getElementById('city-select');
 const refreshButton = document.getElementById('refresh-button');
-
 
 function getWeather(city) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=uk`;
@@ -20,17 +19,42 @@ function getWeather(city) {
             const humidity = data.main.humidity;
             const windSpeed = data.wind.speed;
 
-            weatherInfoDiv.innerHTML = `
-                <p>Місто: ${city}</p>
-                <p>Погода: ${weather}</p>
-                <p>Температура: ${temperature} &#8451;</p>
-                <p>Вологість: ${humidity}%</p>
-                <p>Швидкість вітру: ${windSpeed} м/с</p>
-            `;
+git
+            while (weatherInfoDiv.firstChild) {
+                weatherInfoDiv.removeChild(weatherInfoDiv.firstChild);
+            }
+
+
+            const cityParagraph = document.createElement('p');
+            cityParagraph.textContent = `Місто: ${city}`;
+
+            const weatherParagraph = document.createElement('p');
+            weatherParagraph.textContent = `Погода: ${weather}`;
+
+            const tempParagraph = document.createElement('p');
+            tempParagraph.textContent = `Температура: ${temperature} ℃`;
+
+            const humidityParagraph = document.createElement('p');
+            humidityParagraph.textContent = `Вологість: ${humidity}%`;
+
+            const windParagraph = document.createElement('p');
+            windParagraph.textContent = `Швидкість вітру: ${windSpeed} м/с`;
+
+            weatherInfoDiv.appendChild(cityParagraph);
+            weatherInfoDiv.appendChild(weatherParagraph);
+            weatherInfoDiv.appendChild(tempParagraph);
+            weatherInfoDiv.appendChild(humidityParagraph);
+            weatherInfoDiv.appendChild(windParagraph);
         })
         .catch(error => {
             console.error(error);
-            weatherInfoDiv.innerHTML = `<p>Сталася помилка при отриманні даних для міста ${city}. Перевірте API-ключ або назву міста.</p>`;
+
+            while (weatherInfoDiv.firstChild) {
+                weatherInfoDiv.removeChild(weatherInfoDiv.firstChild);
+            }
+            const errorParagraph = document.createElement('p');
+            errorParagraph.textContent = `Сталася помилка при отриманні даних для міста ${city}. Перевірте API-ключ або назву міста.`;
+            weatherInfoDiv.appendChild(errorParagraph);
         });
 }
 
